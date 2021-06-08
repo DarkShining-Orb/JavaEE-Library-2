fcf89ee34a483f1d5dc4158e34359b07d685a8cd
DarkShining-Orb <269096320@qq.com>
2021-06-08 10:17:50 +0800

    初始化项目

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>我是测试版首页</h1>
+        <el-radio v-model="radio" label="1">备选项</el-radio>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        radio: '1'
+      };
+    }
+  }
+</script>
